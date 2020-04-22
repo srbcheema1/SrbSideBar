3b0de0e6e3c1343893c1e71514c704088f5c66ca
Sarbjit <srbcheema1@gmail.com>
2020-04-22 11:17:24 +0530

    working in simplest form

--- lib/sidebar/sidebar.dart
+++ lib/sidebar/sidebar.dart
@@ -1,165 +1,148 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:rxdart/rxdart.dart';
 
 import '../bloc.navigation_bloc/navigation_bloc.dart';
 import '../sidebar/menu_item.dart';
 
 class SideBar extends StatefulWidget {
+  final _animationDuration = const Duration(milliseconds: 600);
   @override
   _SideBarState createState() => _SideBarState();
 }
 
 class _SideBarState extends State<SideBar> with SingleTickerProviderStateMixin<SideBar> {
+  bool opened;
   AnimationController _animationController;
-  StreamController<bool> isSidebarOpenedStreamController;
-  Stream<bool> isSidebarOpenedStream;
-  StreamSink<bool> isSidebarOpenedSink;
-  final _animationDuration = const Duration(milliseconds: 500);
 
   @override
   void initState() {
     super.initState();
-    _animationController = AnimationController(vsync: this, duration: _animationDuration);
-    isSidebarOpenedStreamController = PublishSubject<bool>();
-    isSidebarOpenedStream = isSidebarOpenedStreamController.stream;
-    isSidebarOpenedSink = isSidebarOpenedStreamController.sink;
+    opened = false;
+    _animationController = AnimationController(vsync: this, duration: widget._animationDuration);
   }
 
   @override
   void dispose() {
     _animationController.dispose();
-    isSidebarOpenedStreamController.close();
-    isSidebarOpenedSink.close();
     super.dispose();
   }
 
   void onIconPressed() {
-    final animationStatus = _animationController.status;
-    final isAnimationCompleted = animationStatus == AnimationStatus.completed;
-
-    if (isAnimationCompleted) {
-      isSidebarOpenedSink.add(false);
+    // final animationStatus = _animationController.status;
+    // final isAnimationCompleted = animationStatus == AnimationStatus.completed;
+    if(opened) {
       _animationController.reverse();
     } else {
-      isSidebarOpenedSink.add(true);
       _animationController.forward();
     }
+    setState(() {opened = !opened;});
   }
 
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
 
-    return StreamBuilder<bool>(
-      initialData: false,
-      stream: isSidebarOpenedStream,
-      builder: (context, isSideBarOpenedAsync) {
-        return AnimatedPositioned(
-          duration: _animationDuration,
-          top: 0,
-          bottom: 0,
-          left: isSideBarOpenedAsync.data ? 0 : -(screenWidth - 55),
-          right: isSideBarOpenedAsync.data ? 10 : screenWidth - 45,
-          child: Row(
-            children: <Widget>[
-              Expanded(
-                child: Container(
-                  padding: const EdgeInsets.fromLTRB(10,0, 10, 0),
-                  alignment: Alignment.topLeft,
-                  color: const Color(0xFF262AAA),
-                  child: ListView(
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(0, 30, 0, 0),
-                        child: CircleAvatar(
-                          child: Icon(Icons.perm_identity,color: Colors.white),
-                          radius: 40.0,
-                        ),
-                      ),
-                      Container(
-                        padding: const EdgeInsets.all(20.0),
-                        alignment: Alignment.center,
-                        child: Text(
-                          "Srb Cheema",
-                          style: TextStyle(color: Colors.white, fontSize: 30, fontWeight: FontWeight.w800),
-                        ),
-                      ),
-                      Divider(
-                        height: 40,
-                        thickness: 0.5,
-                        color: Colors.white.withOpacity(0.3),
-                        indent: 32,
-                        endIndent: 32,
-                      ),
-                      MenuItem(
-                        icon: Icons.home,
-                        title: "Home",
-                        onTap: () {
-                          onIconPressed();
-                          BlocProvider.of<NavigationBloc>(context).add(NavigationEvents.HomePageClickedEvent);
-                        },
-                      ),
-                      MenuItem(
-                        icon: Icons.person,
-                        title: "My Account",
-                        onTap: () {
-                          onIconPressed();
-                          BlocProvider.of<NavigationBloc>(context).add(NavigationEvents.MyAccountClickedEvent);
-                        },
-                      ),
-                      MenuItem(
-                        icon: Icons.shopping_basket,
-                        title: "My Orders",
-                        onTap: () {
-                          onIconPressed();
-                          BlocProvider.of<NavigationBloc>(context).add(NavigationEvents.MyOrdersClickedEvent);
-                        },
-                      ),
-                      Divider(
-                        height: 40,
-                        thickness: 0.5,
-                        color: Colors.white.withOpacity(0.3),
-                        indent: 32,
-                        endIndent: 32,
-                      ),
-                      MenuItem(
-                        icon: Icons.settings,
-                        title: "Settings",
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              Align(
-                alignment: Alignment(0, -0.85),
-                child: GestureDetector(
-                  onTap: () {
-                    onIconPressed();
-                  },
-                  child: ClipPath(
-                    clipper: CustomMenuClipper(),
-                    child: Container(
-                      width: 35,
-                      height: 110,
-                      color: Color(0xFF262AAA),
-                      alignment: Alignment.centerLeft,
-                      child: AnimatedIcon(
-                        progress: _animationController.view,
-                        icon: AnimatedIcons.menu_arrow,
-                        color: Color(0xFF1BB5FD),
-                        size: 25,
-                      ),
+    return AnimatedPositioned(
+      duration: widget._animationDuration,
+      top: 0,
+      bottom: 0,
+      left: opened ? 0 : -(screenWidth - 55),
+      right: opened ? 10 : screenWidth - 45,
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.fromLTRB(10,0, 10, 0),
+              alignment: Alignment.topLeft,
+              color: const Color(0xFF262AAA),
+              child: ListView(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 30, 0, 0),
+                    child: CircleAvatar(
+                      child: Icon(Icons.perm_identity,color: Colors.white),
+                      radius: 40.0,
                     ),
                   ),
+                  Container(
+                    padding: const EdgeInsets.all(20.0),
+                    alignment: Alignment.center,
+                    child: Text(
+                      "srb Cheema",
+                      style: TextStyle(color: Colors.white, fontSize: 30, fontWeight: FontWeight.w800),
+                    ),
+                  ),
+                  Divider(
+                    height: 40,
+                    thickness: 0.5,
+                    color: Colors.white.withOpacity(0.3),
+                    indent: 32,
+                    endIndent: 32,
+                  ),
+                  MenuItem(
+                    icon: Icons.home,
+                    title: "Home",
+                    onTap: () {
+                      onIconPressed();
+                      BlocProvider.of<NavigationBloc>(context).add(NavigationEvents.HomePageClickedEvent);
+                    },
+                  ),
+                  MenuItem(
+                    icon: Icons.person,
+                    title: "My Account",
+                    onTap: () {
+                      onIconPressed();
+                      BlocProvider.of<NavigationBloc>(context).add(NavigationEvents.MyAccountClickedEvent);
+                    },
+                  ),
+                  MenuItem(
+                    icon: Icons.shopping_basket,
+                    title: "My Orders",
+                    onTap: () {
+                      onIconPressed();
+                      BlocProvider.of<NavigationBloc>(context).add(NavigationEvents.MyOrdersClickedEvent);
+                    },
+                  ),
+                  Divider(
+                    height: 40,
+                    thickness: 0.5,
+                    color: Colors.white.withOpacity(0.3),
+                    indent: 32,
+                    endIndent: 32,
+                  ),
+                  MenuItem(
+                    icon: Icons.settings,
+                    title: "Settings",
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment(0, -0.85),
+            child: GestureDetector(
+              onTap: () {
+                onIconPressed();
+              },
+              child: ClipPath(
+                clipper: CustomMenuClipper(),
+                child: Container(
+                  width: 35,
+                  height: 110,
+                  color: Color(0xFF262AAA),
+                  alignment: Alignment.centerLeft,
+                  child: AnimatedIcon(
+                    progress: _animationController.view,
+                    icon: AnimatedIcons.menu_arrow,
+                    color: Color(0xFF1BB5FD),
+                    size: 25,
+                  ),
                 ),
               ),
-            ],
+            ),
           ),
-        );
-      },
+        ],
+      ),
     );
   }
 }
